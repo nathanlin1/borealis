7f25340aee595722498e41073c28e3c1215bf825
nlin12 <nlin12@ucsc.edu>
2024-10-16 00:04:01 -0700

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+// import Auth from '../components/Auth';
+
+const index = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* <Auth /> */}
+      <Text>Welcome</Text>
+    </SafeAreaView>
+  );
+};
+
+export default index;
