3e6b60d43419b730e3c44464cd3cc20d94f09419
Khang <khangt415@gmail.com>
2024-10-22 20:52:54 -0700

    landing page draft

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import LandingPage from './landing';
 // import Auth from '../components/Auth';
 
 const index = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {/* <Auth /> */}
-      <Text>Welcome</Text>
-    </SafeAreaView>
+    // <SafeAreaView style={{ flex: 1 }}>
+    //   {/* <Auth /> */}
+    //   <Text>Welcome</Text>
+    // </SafeAreaView>
+    <LandingPage/>
   );
 };
 
