38f54acb453d4b2fa1ffeb57e71c75d997376fc7
nlin12 <nlin12@ucsc.edu>
2024-10-24 03:25:02 -0700

    routing and oauth

---
+++ app/(routes)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import { View, Text } from "react-native";
+
+const RoutesLayout = () => {
+    return (
+        <Stack/>
+    )
+}
+
+export default RoutesLayout
