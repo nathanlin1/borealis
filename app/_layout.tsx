5e9e31378c91a7dd7bca982c6280be8d0ebf33bb
Aaron Chu <aaronchu@Aarons-Laptop.local>
2024-10-28 18:51:00 -0700

    Updating Settings and Home

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,19 @@
 import { Stack } from "expo-router";
+import GlobalProvider from '../context/GlobalProvider'
 
 const Layout = () => {
-    return <Stack />;
+    return (
+        <GlobalProvider>
+            <Stack>
+                <Stack.Screen name="index" options= {{
+                    headerShown: false
+                }} />
+                <Stack.Screen name="(routes)" options= {{
+                    headerShown: false
+                }} />
+            </Stack>
+        </GlobalProvider>
+    )
 }
 
 export default Layout
